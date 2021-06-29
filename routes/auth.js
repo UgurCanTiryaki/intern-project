d9ce0161778d5680a6f63d6df22beb1be09cc30b
Ugur Can Tiryaki <ugurcantiryaki@hotmail.com>
2021-06-29 16:40:24 +0300

    Create post login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressValidator = require('express-validator');
 
-const { postRegister } = require('../controllers/auth');
+const { postLogin, postRegister } = require('../controllers/auth');
 const { checkValidationResult } = require('../middlewares');
 
 const router = express.Router();
@@ -22,6 +22,24 @@ router.post('/register',
     checkValidationResult
   ],
   postRegister
+);
+
+router.post('/login',
+  express.json(),
+  [
+    expressValidator.body('email')
+      .exists().withMessage('email is required.')
+      .isString().withMessage('email must be a string.')
+      .isEmail().withMessage('email is not valid.'),
+    expressValidator.body('password')
+      .exists().withMessage('password is required.')
+      .isString().withMessage('password must be a string.')
+      .isLength({ min: 8 }).withMessage('password must be minimum 8 characters long.')
+      .isLength({ max: 20 }).withMessage('password must be maximum 20 characters long.')
+      .not().matches(RegExp(/\s/)).withMessage('password must not contain any space character.'),
+    checkValidationResult
+  ],
+  postLogin
 );
 
 module.exports = router;
